d1dfc534529d9cc259963192576aebb13be91a04
Juan Nanio <naniojuan@gmail.com>
2023-12-27 14:35:10 -0300

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -8,12 +9,14 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
-
-  function handleSearch(term: string) {
+  const pathname = usePathname();
+  
+ const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
+
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
         if (term) {
           params.set('query', term);
         } else {
@@ -21,7 +24,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }
         replace(`${pathname}?${params.toString()}`);
 
-  }
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
